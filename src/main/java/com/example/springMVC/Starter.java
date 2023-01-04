6f72a844be9f95a87f8c5c4fd0a28c386cc25286
frb92 <bartek1424@op.pl>
2023-01-04 23:07:40 +0100

    błąd z autowired

--- src/main/java/com/example/springMVC/Starter.java
+++ src/main/java/com/example/springMVC/Starter.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("singleton")
+@Scope("singleton")
 public class Starter implements CommandLineRunner {
 
     @Autowired
